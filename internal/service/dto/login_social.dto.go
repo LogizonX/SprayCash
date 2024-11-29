1a34b885aa4095ef1dc407b5e076e44fe36d6b68
Ayobami Alaran A. <ayobamidele006@gmail.com>
2024-11-30 00:56:21 +0100

    Adds username to social login and registration
    Includes the username field in the social login DTO and updates the user creation process to use the provided username.  This change ensures consistency between regular and social login flows.

--- internal/service/dto/login_social.dto.go
+++ internal/service/dto/login_social.dto.go
@@ -1,6 +1,7 @@
 package dto
 
 type LoginSocialDTO struct {
-	Name  string `json:"name,omitempty"`
-	Email string `json:"email" binding: "required"`
+	Name     string `json:"name,omitempty"`
+	Email    string `json:"email" binding: "required"`
+	Username string `json:"username" binding: "required"`
 }
